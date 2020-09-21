4caed76a0fcb6a75ca527e5ab626b772a1355d8a
DomSykes <65317380+DominicSykes@users.noreply.github.com>
2020-09-20 21:15:30 -0500

    Add files via upload

---
+++ calculating area of a circle Project/Area of a circle.cpp
@@ -0,0 +1,6 @@
+﻿#include <iostream>
+int main() {
+	std::cout << "How do you calculate the area of a circle \n";
+	std::cout << " Well you would Pi r^(2)";
+	return 0;
+} // calculating the area of a circle
